d8fa1b58e9035f154aec4f01f8505205ce05227c
kareninacarrillo85-del <kareninacarrillo85@gmail.com>
2025-11-04 09:56:39 -0300

    Init

---
+++ JS/scripts.js
@@ -0,0 +1,18 @@
+console.log("hola mundo")
+const fecha=document.querySelector(".fecha");
+fecha.textContent=new Date().toLocaleDateString('es-CL');
+const boton = document.getElementById("btnEnviar");
+const campo =document.getElementById("mensaje");
+
+
+
+    
+    
+
+
+
+
+    
+    
+
+
